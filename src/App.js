9ca7391470db6a5ef9f69cb75106ccedbf7e7816
ikenna <enebret@yahoo.com>
2022-03-16 14:16:27 +0100

    css files

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import './App.css';
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 //errors are due to js file extension
 
 function App() {
 {/*logic lives here*/}
 
   return (
-    <Nav variant="pills" defaultActiveKey="/home">
-    <Nav.Item>
-      <Nav.Link href="/home">Active</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav.Item>
-  </Nav>
+    <div>
+    <Container>
+  <div class='top'>
+  <Row>
+    <Col>logo</Col>
+    <Col>
+    <Button variant="outline-success">Login</Button>
+    </Col>
+  </Row>
+  </div>
+</Container>
+<Container>
+  <div class='center-page'>
+   <Row className="justify-content-md-center">
+   <p>ALL ABOUT FTX</p>
+   </Row>
+  </div>
+</Container>
+<Container>
+  <div class='table'>
+
+  </div>
+</Container>
+</div>
   );
 }
 
