700ff471227f196a78d9e80ac85626eecb658400
ikenna <enebret@yahoo.com>
2022-03-17 15:03:24 +0100

    clarity on css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React, {Component} from 'react'
+import * as React from 'react';
 import './App.css';
-import Nav from 'react-bootstrap/Nav'
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 //errors are due to js file extension
 
 function App() {
@@ -13,29 +7,45 @@ function App() {
 
   return (
     <div>
-    <Container>
-  <div class='top'>
-  <Row>
-    <Col>logo</Col>
-    <Col>
-    <Button variant="outline-success">Login</Button>
-    </Col>
-  </Row>
+    <div class="header">
+  <div class ='chania'>
+  <a href='login link'><button type="button">login</button></a>
   </div>
-</Container>
-<Container>
-  <div class='center-page'>
-   <Row className="justify-content-md-center">
-   <p>ALL ABOUT FTX</p>
-   </Row>
-  </div>
-</Container>
-<Container>
-  <div class='table'>
-
-  </div>
-</Container>
+  <h3>logo</h3>
 </div>
+
+<div class="pow">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+      <li>The Flight</li>
+      <li>The City</li>
+      <li>The Island</li>
+      <li>The Food</li>
+    </ul>
+  </div>
+
+  <div class="col-6 col-s-9">
+    <h1>The City</h1>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  </div>
+
+  <div class="col-3 col-s-12">
+    <div class="aside">
+      <h2>What?</h2>
+      <p>Chania is a city on the island of Crete.</p>
+      <h2>Where?</h2>
+      <p>Crete is a Greek island in the Mediterranean Sea.</p>
+      <h2>How?</h2>
+      <p>You can reach Chania airport from all over Europe.</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+
+    </div>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 //banner bg, logo on the left, login on the right
 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
+import './components/login.css';
+
 
