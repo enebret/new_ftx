ec3756b9d89ccca21e7ee2468044d4f48dfa8103
ene ikenna <enebret@yahoo.com>
2022-03-17 21:45:29 +0100

    new components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
 import * as React from 'react';
 import './App.css';
+import Body from './components/body.jsx'
+import Form from './components/login.jsx'
+import { Routes, Route, Link } from "react-router-dom"
 //errors are due to js file extension
-
+/* eslint no-unused-vars : "off" */
 function App() {
 {/*logic lives here*/}
 
   return (
-    <div>
-    <div class="header">
-  <div class ='chania'>
-  <a href='login link'><button type="button">login</button></a>
-  </div>
-  <h3>logo</h3>
-</div>
-
-<div class="pow">
-  <div class="col-3 col-s-3 menu">
-    <ul>
-      <li>The Flight</li>
-      <li>The City</li>
-      <li>The Island</li>
-      <li>The Food</li>
-    </ul>
-  </div>
-
-  <div class="col-6 col-s-9">
-    <h1>The City</h1>
-    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
-  </div>
-
-  <div class="col-3 col-s-12">
-    <div class="aside">
-      <h2>What?</h2>
-      <p>Chania is a city on the island of Crete.</p>
-      <h2>Where?</h2>
-      <p>Crete is a Greek island in the Mediterranean Sea.</p>
-      <h2>How?</h2>
-      <p>You can reach Chania airport from all over Europe.</p>
-    </div>
-  </div>
-</div>
-
-<div class="footer">
-  <p>Resize the browser window to see how the content respond to the resizing.</p>
-</div>
-
-    </div>
+     
+        <div>
+          <Routes>
+          <Route path="/" element={<Body />}></Route>
+          <Route path="about" element={<Form />}></Route>
+        </Routes>
+          
+        </div>
+    
   );
 }
 
---
+++ src/components/body.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom'
+function Body () {
+  const navigate = useNavigate()
+
+    return (
+    
+        <div>
+    <div class="header">
+  <div class ='chania'>
+  <button onClick={() => navigate("about")}>
+        Go Home
+      </button>
+  </div>
+  <h3>logo</h3>
+</div>
+
+<div class="pow">
+  <div class="col-3 col-s-3 menu">
+    <ul>
+      <li>The Flight</li>
+      <li>The City</li>
+      <li>The Island</li>
+      <li>The Food</li>
+    </ul>
+  </div>
+
+  <div class="col-6 col-s-9">
+    <h1>The City</h1>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  </div>
+
+  <div class="col-3 col-s-12">
+    <div class="aside">
+      <h2>What?</h2>
+      <p>Chania is a city on the island of Crete.</p>
+      <h2>Where?</h2>
+      <p>Crete is a Greek island in the Mediterranean Sea.</p>
+      <h2>How?</h2>
+      <p>You can reach Chania airport from all over Europe.</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+    </div>
+    )
+}
+
+export default Body;
