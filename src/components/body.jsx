f6a56b02fc439bbab56f1218c9bec06214d59cd6
ikenna <enebret@yahoo.com>
2022-03-18 15:11:56 +0100

    edits at body.js

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -19,31 +19,22 @@ function Body () {
 
 <div class="pow">
   <div class="col-3 col-s-3 menu">
-    <ul>
-      <li>The Flight</li>
-      <li>The City</li>
-      <li>The Island</li>
-      <li>The Food</li>
-    </ul>
+ 
   </div>
 
-  <div class="col-6 col-s-9">
-    <h1>Table data below this but before register button</h1>
+  <div class="">
+    <div class='cr'>
+    <h1 class="qr">Table data below this but before register button</h1>
     <button id = 'btx' onClick={() => navigate("registration")}>
         Register here
       </button>
-    <h3>All about ftx in a nutshell</h3>
+    <h3 id="gd">All about ftx in a nutshell</h3>
+    </div>
+    
   </div>
 
   <div class="col-3 col-s-12">
-    <div class="aside">
-      <h2>What?</h2>
-      <p>Chania is a city on the island of Crete.</p>
-      <h2>Where?</h2>
-      <p>Crete is a Greek island in the Mediterranean Sea.</p>
-      <h2>How?</h2>
-      <p>You can reach Chania airport from all over Europe.</p>
-    </div>
+  
   </div>
 </div>
 
