f50ffe69fea839c2f3f8832ce3131851d2e6aa03
ikenna <enebret@yahoo.com>
2022-03-23 10:29:36 +0100

    css media query

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -5,20 +5,33 @@ import { useNavigate } from 'react-router-dom';
 
 import Btc from './util/getBtc.js';
 
+import Xrp from './util/getXrp.js';
 
+import Ada from './util/getAda.js';
 
 function Body () {
   const navigate = useNavigate();
   const [dfx, newDfx] = useState(null);
-  //const [rp, newRP] = useState(null);
-  
-  useEffect(() => {
-    setInterval(() => {
+  const [rp, newRP] = useState(null);
+  const [cd, newCd] = useState(null);
+
+  /** setInterval(() => {
       Btc()
     .then(data=>newDfx(data));
-    }, 3000);
+    }, 3200); */
+  useEffect(() => {
+   setInterval(() => {
+    (async()=>{
+      let pg = await Btc();
+      let vg = await Xrp();
+      let cg = await Ada(); 
+      newDfx(pg);
+      newRP(vg);
+      newCd(cg);
+    })();
+   }, 3500);
     
-  }, [dfx]);
+  }, [dfx,rp]);
 
     return (
      <div>
@@ -28,31 +41,45 @@ function Body () {
               </div>
               <h3>logo</h3>
             </div>
+        <div class='tb'>
         <table>
-          <thead></thead>
-          <tbody>
+          <thead>
           <tr>
-           <td>data</td>
-           <td>price</td>
+           <th>data</th>
+           <th>price</th>
           </tr>
+          </thead>
+          <tbody>
+         
           <tr>
           <td>bitcoin</td>
           <td>$  {dfx}</td>
           </tr>
           
+          <tr>
+          <td>ripple</td>
+          <td>$  {rp}</td>
+          </tr>
           
+          <tr>
+          <td>cardano</td>
+          <td>$  {cd}</td>
+          </tr>
+
           </tbody>
         </table>
-        
-       <div class="pow">
-         <div class='cr'>
-           <h1 class="qr">Table data below this but before register button</h1>
-          <button id = 'btx' onClick={() => navigate("registration")}>
+        </div>
+        <button  id = 'tbr' onClick={() => navigate("registration")}>
                  Register here
                         </button>
+       
+         <div class='cr'>
+           <h1 class="qr">Table data below this but before register button</h1>
+          
                  <h3 id="gd">All about ftx in a nutshell</h3>
            </div>
-       </div>
+        
+         
 
       <div class="footer">
            <p>Resize the browser window to see how the content respond to the resizing.</p>
