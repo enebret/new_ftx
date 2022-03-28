0c562f86d41c33a3f2bf0a545b5cac3bd39a7c22
ene ikenna <enebret@yahoo.com>
2022-03-28 11:20:42 +0100

    login form logic

--- src/components/login.js
+++ src/components/login.js
@@ -1,15 +1,36 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import './loginStyles.css';
+import axios from 'axios';
+
 
 function Form () {
+    const [Email, setUserEmail] = useState('');
+    const [Password, setPassword] = useState('');
+   
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const user = {
+            email: Email,
+            password: Password
+          };
+          //console logging here is browser console
+          axios.post('http://localhost:8082/login', user)
+            .then(response => console.log(response.data))
+            
+            .catch(err => {
+              console.error(err);
+          });
+      }
+    
+
     return (
         <div class='pd'>
             <div class = 'fr'>
-            <form>
-              <label for="fname">username</label>
-              <input type="text" placeholder="username.."></input>
-              <label for="fname">password</label>
-              <input type="text" placeholder="password.."></input>
+            <form onSubmit={handleSubmit}>
+              <label for="fname">Enter Email address</label>
+              <input type="text" name ='email' value ={Email} onChange={e => setUserEmail(e.target.value)} />
+              <input type="password" name ='password' value ={Password} onChange={e => setPassword(e.target.value)} />
               <input type="submit" value="Submit"></input>
               </form>
             </div>
