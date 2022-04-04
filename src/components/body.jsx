9d202c1103506b80cfe45e5a50b14537f1075023
ene ikenna <enebret@yahoo.com>
2022-04-04 14:02:46 +0100

    edits

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -12,6 +12,8 @@ import Cg from './coingecko/cg.js';
 
 import logo from './util/logo.JPG';
 
+import usplash from './util/usplash.jpg';
+
 function Body () {
   const navigate = useNavigate();
 
@@ -28,6 +30,11 @@ function Body () {
   const [vbtc, newVBtc] = useState(null);
   const [veth, newVEth] = useState(null);
   const [vrp, newVRp] = useState(null);
+
+  //%change
+  const [cbtc, newCBtc] = useState(null);
+  const [ceth, newCEth] = useState(null);
+  const [crp, newCRp] = useState(null);
 
   useEffect(() => {
    setInterval(() => {
@@ -47,6 +54,11 @@ function Body () {
       let eth_vol = main_object.data[1].vol;
       let rp_vol = main_object.data[2].vol;
 
+      //24hr change
+      let btc_change = main_object.convert[0];
+      let eth_change = main_object.convert[1];
+      let rp_change = main_object.convert[2];
+
       //%
 
       newBtc(plain_btc);
@@ -62,28 +74,44 @@ function Body () {
       newVEth(eth_vol);
       newVRp(rp_vol);
 
+      //
+      newCBtc(btc_change);
+      newCEth(eth_change);
+      newCRp(rp_change);
+
     })();
    }, 5000);
-  }, [btc,eth,rp]);
+  }, [btc,eth,rp, cbtc,ceth,crp]);
 
   
     return (
-     <div>
+     <div class='main'>
           <div class="header">
-            <div class ='chania'>
-               <button id = 'login-button' onClick={() => navigate("form")}>Login</button>
+            
+            <div class ='nav'>
+               <nav>
+                 <img id="lg" src={logo} alt="logo" />
+                 <a href="/html/">HOME</a>
+                 <a href="/html/">COMPANY</a>
+                 <a href="/html/">INVESTMENT</a>
+                 <a href="/html/">FAQ</a>
+                 <a href="/html/">PLATFORM</a>
+                 <a href="/html/">PARTNERSHIP</a>
+                 <a href="/html/">CONTACT US</a>
+                 <button id = 'login-button' onClick={() => navigate("form")}>Login</button>
+               </nav>
               </div>
-              <img class="one" src={logo} alt="logo" />
             </div>
         <div class='tb'>
         <table>
           <thead>
           <tr>
-            <th>logo</th>
-           <th>cryptocurrency</th>
-           <th>price</th>
-           <th>market cap</th>
-           <th>market volume</th>
+            <th>Logo</th>
+           <th>Coin Asset</th>
+           <th>Price</th>
+           <th>Market Cap</th>
+           <th>Volume</th>
+           <th>24hr%</th>
            
           </tr>
           </thead>
@@ -91,41 +119,47 @@ function Body () {
          
           <tr>
           <td><img src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' alt='td' width="40"></img></td>
-          <td>bitcoin</td>
+          <td id = 'ft'>Bitcoin</td>
           <td>{btc}</td>
           <td>{mbtc}</td>
           <td>{vbtc}</td>
+          <td>{cbtc}</td>
           </tr>
           
           <tr>
           <td><img src='https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880' alt='td' width="40"></img></td>
-          <td>ethereum</td>
+          <td id = 'ft'>Ethereum</td>
           <td>{eth}</td>
           <td>{meth}</td>
           <td>{veth}</td>
+          <td>{ceth}</td>
           </tr>
           
           <tr>
           <td><img src='https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731' alt='td' width="40"></img></td>
-          <td>ripple</td>
+          <td id = 'ft'>Ripple</td>
           <td>{rp}</td>
           <td>{mrp}</td>
           <td>{vrp}</td>
+          <td>{crp}</td>
           </tr>
 
           </tbody>
         </table>
         </div>
+        <div class= 'mid_bg_img'>
+        <img id="up" src={usplash} alt="unsplash" />
+          <div class= 'capture_write_up'>
+            <h2>Automate Cryptocurrency trading, forecasting and hedging with our Simple
+            & flexible platform.
+            </h2>
+            <p>Our team of experienced traders does the research and trading for all ours members with our winning business models to skyroket earnings.</p>
+          </div>
+        </div>
         <button  id = 'register-button' onClick={() => navigate("registration")}>
-                 Register here
+                 Get Started Today
                         </button>
        
-         <div class='cr'>
-           <h4 class="first-text">GET DOUBLE DIGIT RETURNS ON EVERY INVESTMENT</h4>
-                 <h7 id="second-text">WE MAKE TRADING EASY AND RELIABLE</h7>
-           </div>
-           
-         
 
       <div class="footer">
            <p>FTX cloud mining is a fully registered company.All rights reserved.</p>
