773a03d8fdfd9ad106820899e1cf7653bd310241
ikenna <enebret@yahoo.com>
2022-05-05 15:30:10 +0100

    edits

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,27 @@
 import './styles/Dropdown.css';
+import { useState } from 'react';
+
+const colors = {
+  Azure: '#151E3D',
+  Gray: '#808080',
+};
 
 function Dropdown(props) {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+      // 👇️ toggle
+      setIsActive(current => !current);
+  
+      // 👇️ or set to true
+      // setIsActive(true);
+    };
+
     return (
-        <div class = 'dd'>
+        <div class = 'dd'  style={{
+          backgroundColor: isActive ? '#F0F0F0' : '',
+        }}
+        onClick={handleClick}>
             
         </div>
     )
