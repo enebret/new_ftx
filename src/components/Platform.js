75717d4bef41ef767477d7961f75397bec49224f
ikenna <enebret@yahoo.com>
2022-05-26 13:24:50 +0100

    edits

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,12 +1,16 @@
 /*jshint esversion: 8 */
 
-
-import firstDiv from './util/aboutusimages/first_div.jpg';
 import './styles/about_us.css';
 import { useNavigate } from 'react-router-dom';
 import logo from './util/logo.JPG';
+import firstDiv from './util/aboutusimages/first_div.jpg';
+import secondDiv from './util/aboutusimages/second_div.jpg';
+import firstPng from './util/aboutusimages/first_png.png';
+import fourthDiv from './util/aboutusimages/fourth_div.jpg';
+import qOption from './util/aboutusimages/q_option.png';
 
-function Platform () {
+
+function AboutUs () {
   const navigate = useNavigate();
 
     return (
@@ -16,13 +20,13 @@ function Platform () {
             <div class ='nav'>
                <nav>
                  <img id="lg" src={logo} alt="logo" />
-                 <a href="#" >HOME</a>
-                 <a >COMPANY</a>
-                 <a href="#">INVESTMENT</a>
-                 <a href="#">FAQ</a>
-                 <a href="#">PLATFORM</a>
-                 <a href="#">PARTNERSHIP</a>
-                 <a href="#">CONTACT US</a>
+                 <a onClick={ () => navigate('/')}>HOME</a>
+                 <a onClick={() => navigate('/aboutus')}>COMPANY</a>
+                 <a onClick={() => navigate('/investment')}>INVESTMENT</a>
+                 <a onClick={() => navigate('/faq')}>FAQ</a>
+                 <a onClick={() => navigate('/platform')}>PLATFORM</a>
+                 <a onClick={() => navigate('/partnership')}>PARTNERSHIP</a>
+                 <a onClick={() => navigate('/contactus')}>CONTACT US</a>
                  <button id = 'login-button' onClick={() => navigate("form")}>Login</button>
                </nav>
               </div>
@@ -30,32 +34,80 @@ function Platform () {
             <div class = 'first_tier_content'>
             <img id="fd" src={firstDiv} alt="firstDiv" />
             <div class = 'firstDivText'>
-              <h2>Investment Plans</h2>
+              <h2>CFD Trading - <strong>Cryptocurrency</strong></h2>
             </div>
+
             </div>
 
-          <div class = 'second_tier_content'>
-   
+          <div class = 'stx'>
+            <div class = 'fext'>
+              <h1>Open Leveraged CFD</h1>
+            </div>
+            <div class = 'dext'>
+              <h1> Trades on Cryptocurrencies</h1>
+            </div>
+            <div class = 'mext'>
+              <p>FXT Trading Limited allows you to gain high levels of exposure with relatively small investment sums, using up to x200 leverage. When you open leveraged positions you are basically investing using FXT Trading Limited`s money and can make much higher profits. We strongly recommend that you visit our education center to learn more about how to use leverage effectively.</p>
+            </div>
+            <div class = 'icons'></div>
           </div>
 
-          <div class = 'third_tier_content'>
-
+          <div class = 'thx'>
+           <div class = 'rst'>
+           <h1>Who Can Trade Cryptocurrencies?</h1>
+           </div>
+           <div class = 'sttc'>
+             <p>Online Cryptocurrency trading is already a highly popular form of investment with millions of traders worldwide. Almost anybody can trade Cryptocurrencies if they are willing to apply the basic principles of risk management. The Arbitrage Compass online Education Center contains all the learning tools and educational resources that you need to begin online Cryptocurrency trading.</p>
+             <p>Many of Trade Max Limited top investors signed up as absolute beginners with no experience of the financial markets. They used the Education Center and free Demo Account to master the basics of Cryptocurrency trading. Cryptocurrency traders range from novice investors who trade part time, to expert investors who manage complex personal portfolios on a full time basis. The Trade Max Limited platform gives you the freedom to set your own financial goals and manage your own investments.</p>
+           </div>
+           <div class = 'imc'>
+           <img id="xfd" src={secondDiv} alt="secondDiv" />
+           </div>
           </div>
 
-          <div class = 'fourth_tier_content'>
-
+          <div class = 'ftc'>
+            <div class ='fng'>
+              <h1>Cryptocurrency Trading</h1>
+            </div>
+            <div class = 'fmg'>
+            <p>By the end of 2016, more than 700 digital currencies were introduced and co-existed. Bearing that in mind, it came as no surprise that during 2017 the total trading volume of the digital currency market has reached a very high value. This phenomenal sum is even more astonishing when considering the simple fact that this market started only 8 years before. A big advantage of digital currencies is that they are not tied to a specific country or bank, therefore allowing them to be liquid not only during traditional trading hours, but also throughout weekend and holidays, allowing people to trade on them 24/7*.</p>
+            </div>
+            <div class = 'ftg'>
+            <img id="xfg" src={firstPng} alt="firstPng" />
+            </div>
           </div>
 
-          <div class = 'fifth_tier_content'>
-
+          <div class = 'ftq'>
+            <div class = 'fch'>  
+              <h2>Why You Need To Be A Member</h2>
+            </div>
+            <div class = 'fcxt'>
+              <p>Unless you trade stocks full-time, you likely do not have the time and/or know-how needed to successfully select the right cannabis, gold, forex and cryptocurrency stock and know when to buy or sell for maximum profits. Our member-only model portfolio shows stocks have been handpicked by experienced financial advisor and experienced stock traders. We will continuously monitor the markets and review current and potential investments. Members will have professional insight at their fingertips.</p>
+            </div>
+            <div class = 'fimg'>
+            <img id="fvx" src={fourthDiv} alt="fourth_div" />
+            </div>
           </div>
 
-          <div class = 'sixth_tier_content'>
-
+          <div class = 'stq'>
+            <div class = 'sgg'>
+              <h1>No Experience Trading, Can I Be A Member</h1>
+            </div>
+            <div class = 'smain'>
+              <p>Everyone has to start somewhere, and FXT Trading is a great place to begin. Our Team od experts clearly lays out the moves his is making in his model portfolio and why. We back it all up with simple-to-understand analysis. Even if you have yet to establish a brokerage account, following along as a member can help give you insight and understanding of the exciting, and often volatile stock industry.</p>
+            </div>
+            <div class = 'sig'>
+            <img id="qnp" src={qOption} alt="q_option" />
+            </div>
           </div>
 
-          <div class = 'seventh_tier_content'>
-
+          <div class = 'svc'>
+            <div class = 'sv_text'>
+              <h3>Click Here to Get</h3>
+            </div>
+            <div class = 'sv_button'>
+            
+            </div>
           </div>
           
             <div class = 'main_footer'>
@@ -118,4 +170,4 @@ function Platform () {
     )
 }
 
-export default Platform;
+export default AboutUs;
