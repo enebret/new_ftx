2128df5bf233a47eb32ab0649ed6e899d5124842
ikenna <enebret@yahoo.com>
2022-05-31 13:28:41 +0100

    edits

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -52,18 +52,88 @@ function Platform () {
               <Row id = 'ftc'>
                 </Row>
               <Row id = 'ftd'>
-            <p>The Trade Max Limited trading platform combines simplicity with sophistication to connect you to the world’s most volatile financial markets. Our dashboard display delivers one-click order execution and is equipped with the latest trading tools. Your trading is backed up by live charts and real-time data feeds, as well as trading signals and the latest market news and analysis.</p>
+            <p>The FTX Limited trading platform combines simplicity with sophistication to connect you to the world’s most volatile financial markets. Our dashboard display delivers one-click order execution and is equipped with the latest trading tools. Your trading is backed up by live charts and real-time data feeds, as well as trading signals and the latest market news and analysis.</p>
               </Row>
 
     </Col>
 
   </Row>
+
   <Row id = 'ftg'>
     <h2>Optimise your Trading
 Experience.</h2>
-    <p>The Trade Max Limited platform is flexible enough to meet every trading style and requirement.</p>
+    <p>The FTX Max Limited platform is flexible enough to meet every trading style and requirement.</p>
   </Row>
  
+
+
+  <Row id = 'fth'>
+      <Col md={{ span: 4, offset: 3 }}>  <Row id = 'fta'>
+              <h2>Economic Calender
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>Create a personal investment schedule and prepare for market volatility around key economic events, news and major decisions. The economic calendar is continually updated and events are rated by importance and relevance.</p>
+              </Row>
+
+    </Col>
+    <Col md={4}>md=4</Col>
+  </Row>
+  
+
+  <Row id = 'fth'>
+      <Col md={4}>md=4</Col>
+
+              <Col md={{ span: 4, offset: 3 }}>  <Row id = 'fta'>
+              <h2>Price Alerts and
+                  Analysis
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>FTX Limited price alerts give you the freedom to step back from the markets until they meet your requirements. Set the rate at which you want to enter the markets and receive immediate notification when it’s time to trade.</p>
+              </Row>
+
+    </Col>
+
+  </Row>
+
+  <Row id = 'fth'>
+      <Col md={{ span: 4, offset: 3 }}>  <Row id = 'fta'>
+              <h2>Risk Management
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>The automatic stop loss, take profit and order entry tools allow you to maximise your profit potential while reducing your risk of loss. The ‘set and forget’ tools will run in the background, relieving you from the need to monitor open trades.</p>
+              </Row>
+
+    </Col>
+    <Col md={4}>md=4</Col>
+  </Row>
+
+  <Row id = 'fth'>
+      <Col md={4}>md=4</Col>
+
+              <Col md={{ span: 4, offset: 3 }}>  <Row id = 'fta'>
+              <h2>Trading Signals
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>Trading signals are provided by one of the financial industry’s leading research houses. They immediately alert you about possible trading opportunities on a real time basis. Although not infallible, trading signals are used by many experienced investors to enhance their own trading strategies.</p>
+              </Row>
+
+    </Col>
+
+  </Row>
+
+
 </Container>
             
             <div class = 'main_footer'>
