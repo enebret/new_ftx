23f8be9c55c42deacaeb38acd904c448d5b18d27
ikenna <enebret@yahoo.com>
2022-06-01 15:25:39 +0100

    edits

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -52,10 +52,34 @@ function Partnership () {
             </Container>
             <Container fluid id = 'rfte'>
               <Row >
-                <p id = 'rftf'>With our affiliate program you can make money by referring clients to TradeMax LTD platform. Promote TradeMax LTD website by placing our banners or text ads on your website, blog or just put the affiliate link in message forum signatures, email signatures or any other places.</p>
-                <p id = 'rftf'>If any visitor clicks on the banner or text ad, opens a new account with TradeMax LTD and makes an investment, you will get a 6% commission from the total of their invested funds. Any additional investment made by your affiliate will generate further 6% commission. Commission is credited to your account balance instantly, without any delays.</p>
+                <p id = 'rftf'>With our affiliate program you can make money by referring clients to FXT LTD platform. Promote FXT LTD website by placing our banners or text ads on your website, blog or just put the affiliate link in message forum signatures, email signatures or any other places.</p>
+                <p id = 'rftf'>If any visitor clicks on the banner or text ad, opens a new account with FXT LTD and makes an investment, you will get a   <span style={{ color: 'green',  fontWeight: '600'}}>6% commission</span> from the total of their invested funds. Any additional investment made by your affiliate will generate further 6% commission. Commission is credited to your account balance instantly, without any delays.</p>
               </Row>
+
+              <Row id = 'rftg'>
+              <Col md={{ span: 5}}>  <Row id = ''>
+              <h2>Trading Signals
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>Trading signals are provided by one of the financial industry’s leading research houses. They immediately alert you about possible trading opportunities on a real time basis. Although not infallible, trading signals are used by many experienced investors to enhance their own trading strategies.</p>
+              </Row>
+            </Col>
+            <Col md={4}></Col>
+          </Row>
+          <Row md={5} id = 'btr4'>
+              <Button variant="primary" type="submit"  style={{ backgroundColor: 'green',  fontWeight: '600'}}>
+                  GET STARTED TODAY
+              </Button>
+              </Row>
+
+
+
             </Container>
+
+
             <Container id = 'fmain'>
               <Row>
 
