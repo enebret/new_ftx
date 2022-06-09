4a3daf6c19b73fabc7dcc6dfdaf773cdb0599c74
ikenna <enebret@yahoo.com>
2022-06-09 11:34:41 +0100

    edits

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -20,7 +20,7 @@ import Accordion from 'react-bootstrap/Accordion';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Form from 'react-bootstrap/Form';
 
 
 
@@ -42,19 +42,36 @@ function Body () {
       <Nav.Link onClick={() => navigate('/platform')}>Platform</Nav.Link>
       <Nav.Link onClick={() => navigate('/partnership')}>Partnership</Nav.Link>
       <Nav.Link onClick={() => navigate('/contactus')}>Contact us</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+ 
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+    <Nav id = 'fr'>
+    <NavDropdown title="My Account" id="collasible-nav-dropdown" >
+      <Form id = 'fc'>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Signin
+  </Button>
+  <Form.Group id = 'txy' >
+  <Form.Text >
+  Don't have an account yet? <a href='' onClick={() => navigate('/contactus')}>CREATE AN ACCOUNT NOW</a>
+    </Form.Text>
+  </Form.Group>
+</Form>
+      </NavDropdown>
     </Nav>
   </Navbar.Collapse>
   </Container>
