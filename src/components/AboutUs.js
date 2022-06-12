b5948cc171ddaae7f424bf46375e48d30ff229ca
ene ikenna <enebret@yahoo.com>
2022-06-12 19:01:15 +0100

    edits

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -97,7 +97,7 @@ function Platform () {
        </Row>
       
        <Row id = 'ftd'>
-     <p style={{ lineHeight: "2" }}>FTX Limited allows you to gain high levels of exposure with relatively small investment sums, using up to x200 leverage. When you open leveraged positions you are basically investing using Trade Max Limited`s money and can make much higher profits. We strongly recommend that you visit our education center to learn more about how to use leverage effectively.</p>
+     <p style={{ lineHeight: "2", textAlign: "justify"}}>FTX Limited allows you to gain high levels of exposure with relatively small investment sums, using up to x200 leverage. When you open leveraged positions you are basically investing using FTX Limited`s money and can make much higher profits. We strongly recommend that you visit our education center to learn more about how to use leverage effectively.</p>
        </Row>
        <img src= '/pix/btc.png' style={{ marginLeft: "5%" }}/>      
        <img src= '/pix/rp.png' style={{ marginLeft: "5%" }}/>
@@ -109,73 +109,79 @@ function Platform () {
 </Row>
 </Container>
 
-<Container  fluid  style={{ padding: "5%" }}>
+<Container  fluid  style={{ paddingLeft: "10%", paddingRight: '10%', paddingBottom: '10%', lineHeight: "2", marginTop: '5%'  }}>
 <Row style={{ margin: "auto" }}>
       <Col md={{ span: 6, }}>  <Row id = 'fta'>
               <h2>Who Can Trade Cryptocurrencies?</h2> 
               </Row>
               
-              <Row style={{ lineHeight: "2" }}>
+              <Row style={{ lineHeight: "2", textAlign: "justify"  }}>
             <p>Online Cryptocurrency trading is already a highly popular form of investment with millions of traders worldwide. Almost anybody can trade Cryptocurrencies if they are willing to apply the basic principles of risk management. The Arbitrage Compass online Education Center contains all the learning tools and educational resources that you need to begin online Cryptocurrency trading.</p>
-            <p> Many of Trade Max Limited top investors signed up as absolute beginners with no experience of the financial markets. They used the Education Center and free Demo Account to master the basics of Cryptocurrency trading. Cryptocurrency traders range from novice investors who trade part time, to expert investors who manage complex personal portfolios on a full time basis. The Trade Max Limited platform gives you the freedom to set your own financial goals and manage your own investments.</p>
+            <p> Many of FTX Limited top investors signed up as absolute beginners with no experience of the financial markets. They used the Education Center and free Demo Account to master the basics of Cryptocurrency trading. Cryptocurrency traders range from novice investors who trade part time, to expert investors who manage complex personal portfolios on a full time basis. The FTX Limited platform gives you the freedom to set your own financial goals and manage your own investments.</p>
               </Row>
 
     </Col>
-    <Col md={6}><Row><img id="npq" src={secondDiv} alt="q_option" /></Row></Col>
+    <Col md={6} style={{ marginTop: "5%" }}><Row><img id="npq" src={secondDiv} alt="q_option" /></Row></Col>
   </Row>
   
 </Container>
 
-<Container  fluid  style={{ padding: "5%", lineHeight: "2" }}>
-<Row >
-      <Col md={6}><Row><img id="npq" src={fourthDiv} alt="q_option" /></Row></Col>
+<Container  fluid  style={{ paddingLeft: "10%", paddingRight: '10%', paddingBottom: '10%', lineHeight: "2"  }}>
+<Row style={{ margin: "auto" }}>
+      
 
               <Col md={{ span: 6}}>  <Row id = 'fta'>
-              <h2>Price Alerts and
-                  Analysis
+              <h2>Cryptocurrency Trading
                 </h2> 
               </Row>
               <Row id = 'ftc'>
                 </Row>
-              <Row id = 'ftd'>
-            <p>FTX Limited price alerts give you the freedom to step back from the markets until they meet your requirements. Set the rate at which you want to enter the markets and receive immediate notification when it’s time to trade.</p>
+              <Row id = 'ftd'   style={{  textAlign: "justify"  }}>
+            <p>By the end of 2016, more than 700 digital currencies were introduced and co-existed. Bearing that in mind, it came as no surprise that during 2017 the total trading volume of the digital currency market has reached a very high value. This phenomenal sum is even more astonishing when considering the simple fact that this market started only 8 years before. A big advantage of digital currencies is that they are not tied to a specific country or bank, therefore allowing them to be liquid not only during traditional trading hours, but also throughout weekend and holidays, allowing people to trade on them 24/7*.</p>
               </Row>
 
     </Col>
 
+    <Col md={6} style={{ marginTop: "7%" }}><Row><img id="npq" src= '/pix/dv.png' alt="q_option" /></Row></Col>
+
   </Row>
 </Container>
 
-<Container  style={{ padding: "5%", lineHeight: "2" }}>
-<Row>
-      <Col md={{ span: 6}}>  <Row id = 'fta'>
-              <h2>Risk Management
-                </h2> 
-              </Row>
-              <Row id = 'ftc'>
-                </Row>
-              <Row id = 'ftd'>
-            <p>The automatic stop loss, take profit and order entry tools allow you to maximise your profit potential while reducing your risk of loss. The ‘set and forget’ tools will run in the background, relieving you from the need to monitor open trades.</p>
-              </Row>
-
-    </Col>
-    <Col md={6}><Row><img id="npq" src={firstPng} alt="q_option" /></Row></Col>
-  </Row>
-</Container>
-
-<Container fluid  style={{ padding: "5%", lineHeight: "2"  }}>
+<Container fluid  style={{ paddingLeft: "10%", paddingRight: '10%', paddingBottom: '10%', lineHeight: "2"   }}>
   
   <Row >
-      <Col md={6}><Row><img id="npq" src={qOption} alt="q_option" /></Row></Col>
+      <Col md={5}><Row><img id="npq" src= '/pix/mem.jpg' alt="q_option" /></Row></Col>
 
-              <Col md={{ span: 6}}>  <Row id = 'fta'>
-              <h2>Trading Signals
+              <Col md={{ span: 7}}  style={{ marginTop: "5%", textAlign: "justify"  }}>  <Row id = 'fta'>
+              <h2>Why You Need To Be A Member
                 </h2> 
               </Row>
               <Row id = 'ftc'>
                 </Row>
               <Row id = 'ftd'>
-            <p>Trading signals are provided by one of the financial industry’s leading research houses. They immediately alert you about possible trading opportunities on a real time basis. Although not infallible, trading signals are used by many experienced investors to enhance their own trading strategies.</p>
+            <p>Unless you trade stocks full-time, you likely do not have the time and/or know-how needed to successfully select the right cannabis, gold, forex and cryptocurrency stock and know when to buy or sell for maximum profits. Our member-only model portfolio shows stocks have been handpicked by experienced financial advisor and experienced stock traders. We will continuously monitor the markets and review current and potential investments. Members will have professional insight at their fingertips.</p>
+              </Row>
+
+    </Col>
+
+  </Row>
+
+
+</Container>
+
+<Container fluid  style={{ paddingLeft: "10%", paddingRight: '10%', paddingBottom: '10%', lineHeight: "2"  }}>
+  
+  <Row >
+      <Col md={6} style={{ marginTop: "5%" }}><Row><img id="npq" src= '/pix/ph.png' alt="q_option" /></Row></Col>
+
+              <Col md={{ span: 6}}  style={{ textAlign: "justify" }}>  <Row id = 'fta'>
+              <h2>No Experience Trading, Can I Be A Member
+                </h2> 
+              </Row>
+              <Row id = 'ftc'>
+                </Row>
+              <Row id = 'ftd'>
+            <p>Everyone has to start somewhere, and FTX LTD is a great place to begin. Our Team od experts clearly lays out the moves his is making in his model portfolio and why. We back it all up with simple-to-understand analysis. Even if you have yet to establish a brokerage account, following along as a member can help give you insight and understanding of the exciting, and often volatile stock industry.</p>
               </Row>
 
     </Col>
