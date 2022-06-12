81a175dbef07a6b35a768576deee3a5a1a100cbc
ene ikenna <enebret@yahoo.com>
2022-06-12 09:50:27 +0100

    edits

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 8 */
 
-import './styles/platform.css';
+import './styles/aboutus.css';
 import { useNavigate } from 'react-router-dom';
 import logo from './util/logo.JPG';
 import firstDiv from './util/aboutusimages/first_div.jpg';
@@ -19,8 +19,8 @@ import Accordion from 'react-bootstrap/Accordion';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
-
-
+import Form from 'react-bootstrap/Form';
+import ab from './pix/lapy.jpg';
 
 
 
@@ -34,7 +34,7 @@ function Platform () {
         
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">FTX</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
@@ -45,28 +45,45 @@ function Platform () {
       <Nav.Link onClick={() => navigate('/platform')}>Platform</Nav.Link>
       <Nav.Link onClick={() => navigate('/partnership')}>Partnership</Nav.Link>
       <Nav.Link onClick={() => navigate('/contactus')}>Contact us</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
+ 
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+    <Nav id = 'fr'>
+    <NavDropdown title="My Account" id="collasible-nav-dropdown" >
+      <Form id = 'fc'>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Signin
+  </Button>
+  <Form.Group id = 'txy' >
+  <Form.Text >
+  Don't have an account yet? <a href='' onClick={() => navigate('/signup')}>CREATE AN ACCOUNT NOW</a>
+    </Form.Text>
+  </Form.Group>
+</Form>
+      </NavDropdown>
     </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
 
 
-            <Container fluid id = 'ftx'>
+            <Container fluid id = 'ftx' style={{ backgroundImage: `url(${ab})` }}>
               <Row>
-            <h2>CFD Trading - Cryptocurrency</h2>
+            <h2 style={{ color: "white" }}>CFD Trading - Cryptocurrency</h2>
               </Row>
             </Container>
     
